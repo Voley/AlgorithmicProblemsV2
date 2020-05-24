7e4ba9669824c49b6f1b2cbf91bb0d80e12d263f
Dmitry Volevodz <dimavolevodz@gmail.com>
2020-05-24 21:39:57 +0300

    Added 328: Even Odd Partition

--- LinkedLists/EvenOdd/Solution.py
+++ LinkedLists/EvenOdd/Solution.py
@@ -1,2 +1,50 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def print(self):
+        print("Node(" + str(self.val) + ")", end="")
+        if self.next is not None:
+            print(" -> ", end ="")
+            self.next.print()
 
-print("Hello, python")
+# Problem code
+
+def oddEvenList(head: ListNode) -> ListNode:
+    even_head = even_cur = ListNode()
+    odd_head = odd_cur = ListNode()
+    counter = 0
+
+    while head:
+        if counter % 2 == 0:
+            even_cur.next = head
+            even_cur = head
+        else:
+            odd_cur.next = head
+            odd_cur = head
+        head = head.next
+        counter += 1
+    even_cur.next = odd_head.next
+    odd_cur.next = None
+    return even_head.next
+
+
+# Setup
+a = ListNode(0)
+b = ListNode(1)
+c = ListNode(2)
+d = ListNode(3)
+e = ListNode(4)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+print("Created a list:")
+print("")
+a.print()
+print("\n")
+print("After code:")
+print("\n")
+result = oddEvenList(a)
+result.print()
--- LinkedLists/EvenOdd/Solution.swift
+++ LinkedLists/EvenOdd/Solution.swift
@@ -1,1 +1,63 @@
-print("Hello, swift")
+
+public class ListNode {
+  public var val: Int
+  public var next: ListNode?
+  public init() { self.val = 0; self.next = nil; }
+  public init(_ val: Int) { self.val = val; self.next = nil; }
+  public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+  public func printList() {
+    print("Node(\(self.val))", terminator: "")
+    if let next = self.next {
+      print(" -> ", terminator: "")
+      next.printList()
+    }
+  }
+}
+// Problem code
+func oddEvenList(_ head: ListNode?) -> ListNode? {
+  let even_head = ListNode()
+  var even_cur: ListNode? = even_head
+  let odd_head = ListNode()
+  var odd_cur: ListNode? = odd_head
+  var counter = 0
+  var temp = head
+
+  while let h = temp {
+    if counter % 2 == 0 {
+      even_cur?.next = h
+      even_cur = h
+    } else {
+      odd_cur?.next = h
+      odd_cur = h
+    }
+    temp = h.next
+    counter += 1
+  }
+  even_cur?.next = odd_head.next
+  odd_cur?.next = nil
+  return even_head.next
+}
+
+// Setup
+print("Setting up a list:")
+var a = ListNode(0)
+var b = ListNode(1)
+var c = ListNode(2)
+var d = ListNode(3)
+var e = ListNode(4)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+a.printList()
+print("")
+
+print("Running code...")
+print("")
+let result = oddEvenList(a)
+print("Resulting list:")
+if let result = result {
+  result.printList()
+}
